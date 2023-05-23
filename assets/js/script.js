6c158e0db1f9586743a6845916a73fab6ba830c1
sukreth116 <sukrethom2000@gmail.com>
2023-05-23 12:28:41 +0530

    first

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+                            
+    $('#btnSubmit').click(function () {
+        $('#myAlert').show('fade');
+    });
+
+    $('#linkClose').click(function () {
+        $('#myAlert').hide('fade');
+    });
+});
